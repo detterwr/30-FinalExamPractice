78c0bd1de2d6f3ebbd0e05a881044b95c3dd796b
detterwr <detterwr@rose-hulman.edu>
2018-05-23 19:45:52 -0400

    DONE

--- src/m5_loops_within_loops_sequences.py
+++ src/m5_loops_within_loops_sequences.py
@@ -5,8 +5,8 @@ This problem provides practice at:
   ***  LOOPS WITHIN LOOPS in SEQUENCES-OF-SUBSEQUENCES problems.  ***
 
 Authors: David Mutchler, Valerie Galluzzi, Mark Hays, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         their colleagues and Will Detterman.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 ########################################################################
 # Students:
@@ -38,7 +38,7 @@ def main():
 def run_test_integers():
     """ Tests the    integers    function. """
     # ------------------------------------------------------------------
-    # TODO: 2. Implement this TEST function.
+    # DONE: 2. Implement this TEST function.
     #   It TESTS the  integers  function defined below.
     #   Include at least ** 1 ** ADDITIONAL test beyond those we wrote.
     #
@@ -67,6 +67,15 @@ def run_test_integers():
                        ['oops'],
                        [[55], [44]],
                        [30, -4]
+                       ])
+    print('Expected is:', expected)
+    print('Actual is:  ', answer)
+
+    # Test 2:
+    expected = [3, 4 ,6]
+    answer = integers([(3, "featherdogs"),
+                       ("elevendy2", 'hi', 4),
+                       ["one", 6, 'iguana']
                        ])
     print('Expected is:', expected)
     print('Actual is:  ', answer)
@@ -119,11 +128,20 @@ def integers(sequence_of_sequences):
     #    TIME ESTIMATE:  10 minutes.
     # ------------------------------------------------------------------
 
+    list = []
+    for k in range(len(sequence_of_sequences)):
+        for l in range(len(sequence_of_sequences[k])):
+            if type((sequence_of_sequences[k])[l]) == int:
+                list.append((sequence_of_sequences[k])[l])
+    return list
+
+
+
 
 def run_test_big_letters():
     """ Tests the    big_letters    function. """
     # ------------------------------------------------------------------
-    # TODO: 4. Implement this TEST function.
+    # DONE: 4. Implement this TEST function.
     #   It TESTS the  big_letters  function defined below.
     #   Include at least ** 1 ** ADDITIONAL test beyond those we wrote.
     # ------------------------------------------------------------------
@@ -160,6 +178,16 @@ def run_test_big_letters():
     print('Actual is:  ', answer)
 
 
+    # Test 2:
+    expected = 'F'
+    answer = big_letters([(3, 1, 4),  # not a string
+                          'why are there no Fat cats?',  # OTSSS
+                          [(10, 'Even the elephAnTs HavE tRunks', "DOGS")],  # not a string
+                          ("microwave stadium", 19281)])
+    print('Expected is:', expected)
+    print('Actual is:  ', answer)
+
+
 def big_letters(sequence_of_sequences):
     """
     Returns a new STRING that contains all the upper-case letters
@@ -189,7 +217,7 @@ def big_letters(sequence_of_sequences):
     Precondition:  the given argument is a sequence of sequences.
     """
     # ------------------------------------------------------------------
-    # TODO: 5. Implement and test this function.
+    # DONE: 5. Implement and test this function.
     #   Note that you should write its TEST function first (above).
     # ------------------------------------------------------------------
     ####################################################################
@@ -211,6 +239,16 @@ def big_letters(sequence_of_sequences):
     #    DIFFICULTY:      7
     #    TIME ESTIMATE:  12 minutes.
     # ------------------------------------------------------------------
+
+
+    "dogs".isupper()
+    retval = ""
+    for k in range(len(sequence_of_sequences)):
+        for l in range(len(sequence_of_sequences[k])):
+            if type(sequence_of_sequences[k]) == str:
+                if (sequence_of_sequences[k])[l].isupper():
+                    retval = retval + (sequence_of_sequences[k])[l]
+    return retval
 
 # ----------------------------------------------------------------------
 # Calls  main  to start the ball rolling.
